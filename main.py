00594bac5ee808645bc6bf2425a52ce6d6415f5a
cwh <11945090+a-rain-of-leaves-and-stars@user.noreply.gitee.com>
2026-06-24 23:56:33 +0800

    踏上仙途

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import logging
+
 from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star
 from astrbot.core.star.filter.event_message_type import EventMessageType
@@ -11,7 +13,10 @@ class QiuXian(Star):
     async def createUser(self, event: AstrMessageEvent):
         user_name = event.get_sender_name()
         message_str = event.message_str # 获取消息的纯文本内容
+        group_id = event.get_group_id()
         set_state(event.get_sender_id(), "setUserName")
+        set_temp(event.get_sender_id(), "group_id", group_id)
+        logging.info(f"用户: {user_name}，群组: {group_id}","触发创建角色命令")
         yield event.plain_result(f"玩家: {user_name}，请输入您的角色名称：") # 发送一条纯文本消息
 
     @filter.event_message_type(EventMessageType.ALL)
